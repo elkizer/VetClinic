16c5e3eed643b195cad15d9bb513694c607ed2a3
Emily Kizer <ekizer@fdsrx.com>
2021-01-01 23:34:50 -0500

    Add appointments

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -13,11 +13,12 @@ namespace VetClinic.Models
         public long EmployeeId { get; set; }
         
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:f}", ApplyFormatInEditMode = true)]
         [Display(Name = "Appointment Date")]
         public DateTime AppointmentDate { get; set; }
         public string Reason { get; set; }
 
+        [Display(Name = "Pet")]
         public virtual ClientAnimal ClientAnimal { get; set; }
         public virtual Employee Employee { get; set; }
     }
